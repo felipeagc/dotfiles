e787988314bdb4ccbb44f7f22a697598c70c3f2b
Felipe A <felipeac@protonmail.com>
2018-01-10 20:30:19 -0200

    Remove unused configs

--- scripts/.scripts/gamemode.sh
+++
@@ -1,8 +0,0 @@
-value=$(nvidia-settings -q CurrentMetaMode | grep -ic ForceCompositionPipeline=On)
-if [ $value -eq 0 ]
-then
-    echo hello
-    nvidia-settings --assign "CurrentMetaMode=DPY-5: nvidia-auto-select +0+0 { ForceFullCompositionPipeline = On }"
-else
-    nvidia-settings --assign "CurrentMetaMode=DPY-5: nvidia-auto-select +0+0 { ForceFullCompositionPipeline = Off }"
-fi
