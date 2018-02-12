7e49c87e47240640966b3f730fa614f4bd1a02b5
Felipe A <felipeac@protonmail.com>
2018-02-12 00:27:58 -0200

    Update polybar config

--- polybar/.config/polybar/coin.py
+++ polybar/.config/polybar/coin.py
@@ -1,87 +1,38 @@
 #!/usr/bin/env python3
 
 import sys
-import os
 import requests
 
 
-class Coin:
-    def __init__(self, name, prefix):
-        self.name = name
-        self.prefix = prefix
+if __name__ == "__main__":
+    coin1 = sys.argv[1].upper()
+    coin2 = sys.argv[2].upper()
 
-    def get_price(self):
-        response = requests.get('https://api.coinmarketcap.com/v1/ticker/' +
-                                self.name + '/')
-        ticker = response.json()
+    url = f"https://min-api.cryptocompare.com/data/pricemultifull?fsyms={coin1}&tsyms={coin2}"
 
-        price = float(ticker[0]['price_usd'])
-        return price
-
-    def get_text(self):
-        response = requests.get('https://api.coinmarketcap.com/v1/ticker/' +
-                                self.name + '/')
-        ticker = response.json()
-
-        price = float(ticker[0]['price_usd'])
-        change = float(ticker[0]['percent_change_24h'])
-        change_text = ''
-
-        if change >= 0:
-            change_text = '%{F#98c379}' + ('%.2f' % change) + '%'
-        else:
-            change_text = '%{F#be5046}' + ('%.2f' % change) + '%'
-
-        text = (coin.prefix + ' $%.2f (%s%%{F-})') % (price, change_text)
-        return text
-
-
-coins = {
-    "btc": Coin("bitcoin", "BTC"),
-    "bch": Coin("bitcoin-cash", "BCH"),
-    "eth": Coin("ethereum", "ETH"),
-    "xmr": Coin("monero", "XMR")
-}
-
-arg = sys.argv[1]
-if arg == "ratio":
     try:
-        ratio = float(requests.get('https://shapeshift.io/rate/bch_eth')
-                      .json()["rate"])
-        last_ratio = 0
-
-        if ratio <= 0:
-            quit()
-
-        if not os.path.exists("/tmp/coin_ratio.txt"):
-            with open("/tmp/coin_ratio.txt", "w") as tmpfile:
-                pass
-
-        with open("/tmp/coin_ratio.txt", "r") as tmpfile:
-            lines = tmpfile.readlines()
-            if len(lines) > 0:
-                last_ratio = float(lines[len(lines)-1])
-
-        with open("/tmp/coin_ratio.txt", "a") as tmpfile:
-            if last_ratio != ratio:
-                tmpfile.write(str(ratio) + "\n")
-
-        change = (-1 + (ratio/last_ratio)) * 100
-
-        if abs(change) < 0.00001:
-            quit()
-
-        change_text = ''
+        json = requests.get(url).json()
+        price = float(json["RAW"][coin1][coin2]["PRICE"])
+        change = float(json["RAW"][coin1][coin2]["CHANGEPCT24HOUR"])
 
         if change >= 0:
-            change_text = '%{F#98c379}' + ('%.2f' % change) + '%'
+            change_text = '%{F#a1b56c}' + ('%.2f' % change) + '%'
         else:
-            change_text = '%{F#be5046}' + ('%.2f' % change) + '%'
+            change_text = '%{F#ab4642}' + ('%.2f' % change) + '%'
 
-        text = ("BCH/ETH %.3f (%s%%{F-})") % (ratio, change_text)
+        suffix = ""
+        prefix = ""
+
+        if coin2 == "USD":
+            prefix = "$"
+        elif coin2 == "EUR":
+            prefix = "€"
+        else:
+            suffix = coin2
+
+        text = (coin1 + ' ' + prefix +
+                '%.2f' + suffix + ' (%s%%{F-})') % (price, change_text)
+
         print(text)
     except Exception:
         pass
-else:
-    coin = coins[arg]
-    print(coin.get_text())
