ac390266601b68d0934952ffacd06fb23212d67d
Felipe <felipeagc2@gmail.com>
2020-06-19 23:34:30 -0300

    Add custom c script for dwm status

--- dwm/.dwm/config.h
+++ dwm/.dwm/config.h
@@ -79,8 +79,6 @@ static Key keys[] = {
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_m,      zoom,           {0} },
