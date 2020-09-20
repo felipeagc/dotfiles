ac5b41d7f7bbabb69dd3a5839c50b6bfd9805f7f
Felipe Costa <felipeagc2@gmail.com>
2020-09-20 02:16:29 -0300

    Update emacs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -185,6 +185,11 @@
   :after evil
   :config
   (evil-commentary-mode))
+
+(use-package evil-lion
+  :after evil
+  :config
+  (evil-lion-mode))
 ;; }}}
 
 ;; Theme {{{
@@ -344,7 +349,12 @@
   :config
   (setq compilation-read-command nil ; Stops projectile from asking for compile command
         projectile-completion-system 'ivy)
-  (projectile-global-mode))
+  (projectile-global-mode)
+
+  (projectile-register-project-type 'dlang-dub '("dub.sdl")
+                                    :project-file "dub.sdl"
+                                    :compile "dub build")
+  )
 ;; }}}
 
 ;; Highlight todo {{{
@@ -360,39 +370,6 @@
 
 (add-to-list 'auto-mode-alist '("\\.inl\\'" . c-mode))
 
-;; Highlight function names
-(add-hook 'c-mode-common-hook (lambda ()
-    (font-lock-add-keywords nil '(
-        ; Valid hex number (will highlight invalid suffix though)
-        ("\\b0x[[:xdigit:]]+[uUlL]*\\b" . font-lock-string-face)
-
-        ; Invalid hex number
-        ("\\b0x\\(\\w\\|\\.\\)+\\b" . font-lock-warning-face)
-
-        ; Valid floating point number.
-        ("\\(\\b[0-9]+\\|\\)\\(\\.\\)\\([0-9]+\\(e[-]?[0-9]+\\)?\\([lL]?\\|[dD]?[fF]?\\)\\)\\b" (1 font-lock-string-face) (3 font-lock-string-face))
-
-        ; Invalid floating point number.  Must be before valid decimal.
-        ("\\b[0-9].*?\\..+?\\b" . font-lock-warning-face)
-
-        ; Valid decimal number.  Must be before octal regexes otherwise 0 and 0l
-        ; will be highlighted as errors.  Will highlight invalid suffix though.
-        ("\\b\\(\\(0\\|[1-9][0-9]*\\)[uUlL]*\\)\\b" 1 font-lock-string-face)
-
-        ; Valid octal number
-        ("\\b0[0-7]+[uUlL]*\\b" . font-lock-string-face)
-
-        ; Floating point number with no digits after the period.  This must be
-        ; after the invalid numbers, otherwise it will "steal" some invalid
-        ; numbers and highlight them as valid.
-        ("\\b\\([0-9]+\\)\\." (1 font-lock-string-face))
-
-        ; Invalid number.  Must be last so it only highlights anything not
-        ; matched above.
-        ("\\b[0-9]\\(\\w\\|\\.\\)+?\\b" . font-lock-warning-face)
-    ))
-))
-
 (defun felipe/c-c++-hook ()
   (when (boundp 'company-backends)
     (make-local-variable 'company-backends)
@@ -407,6 +384,10 @@
   "mf" 'clang-format-buffer)
 (felipe/leader-def 'normal c++-mode-map
   "mf" 'clang-format-buffer)
+;; }}}
+
+;; D {{{
+(use-package d-mode)
 ;; }}}
 
 ;; GLSL {{{
@@ -429,7 +410,6 @@
 ;; }}}
 
 ;; Other major modes {{{
-(use-package d-mode)
 (use-package org)
 (use-package yaml-mode)
 ;; }}}
