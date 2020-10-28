28805a2fcbb922c7b2489a467a5e07baf104a2ea
Felipe <felipeagc2@gmail.com>
2020-10-28 18:09:07 -0300

    Change st font to Hack

--- st/.st/config.h
+++ st/.st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hasklig:pixelsize=14:antialias=true:autohint=true";
+static char *font = "Hack:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
