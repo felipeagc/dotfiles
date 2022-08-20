dda46f2a946a094ac738784f5ed5579a95c7dec2
Felipe A. Costa <felipeagc2@gmail.com>
2022-08-20 04:15:57 -0300

    Add nix home-manager config

--- nix/.config/nixpkgs/home.nix
+++ nix/.config/nixpkgs/home.nix
@@ -1,4 +1,24 @@
-{ pkgs, ...}:
+{ config, pkgs, ... }:
+
 {
-  targets.genericLinux.enable = true;
+  programs.direnv.enable = true;
+  programs.direnv.nix-direnv.enable = true;
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "felipe";
+  home.homeDirectory = "/home/felipe";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
 }
