4b38694dbe1fe934a5304ae7e52af50de4eb11c7
Felipe A. Costa <felipeagc2@gmail.com>
2023-09-08 21:58:39 -0300

    Update emacs config

--- emacs/.emacs.d/early-init.el
+++ emacs/.emacs.d/early-init.el
@@ -1,3 +1,24 @@
-(setq package-enable-at-startup nil ; don't auto-initialize!
-      package--init-file-ensured t  ; don't add that `custom-set-variables' block to my init.el!
-      gc-cons-threshold 100000000)
+;; Startup speed, annoyance suppression
+(setq gc-cons-threshold 10000000)
+(setq byte-compile-warnings '(not obsolete))
+(setq warning-suppress-log-types '((comp) (bytecomp)))
+(setq native-comp-async-report-warnings-errors 'silent)
+
+;; Silence stupid startup message
+(setq inhibit-startup-echo-area-message (user-login-name))
+
+(setq package-enable-at-startup nil)
+
+(setq frame-resize-pixelwise t)
+
+(setq default-frame-alist '((maximized)
+
+                            ;; You can turn off scroll bars by uncommenting these lines:
+                            ;; (vertical-scroll-bars . nil)
+                            ;; (horizontal-scroll-bars . nil)
+
+                            ;; Setting the face in here prevents flashes of
+                            ;; color as the theme gets activated
+                            (background-color . "#000000")
+                            (ns-appearance . dark)
+                            (ns-transparent-titlebar . t)))
--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,3 +1,77 @@
+(defconst -is-a-mac (eq system-type 'darwin))
+
+;; Backups
+(setq
+  backup-by-copying t      ; don't clobber symlinks
+  backup-directory-alist '(("." . "~/.emacs.d/saves/"))    ; don't litter my fs tree
+  delete-old-versions t
+  kept-new-versions 6
+  kept-old-versions 2
+  version-control t
+  global-visual-line-mode t
+  visible-bell 1)
+
+;; Font
+(add-to-list 'default-frame-alist '(font . "Menlo-13"))
+
+;; Visuals
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(menu-bar-mode -1)
+(blink-cursor-mode 0)
+
+;; Improve macOS stuff
+(when -is-a-mac
+    (setq mac-command-modifier 'meta)
+    (setq mac-option-modifier 'none)
+    ;; Make mouse wheel / trackpad scrolling less jerky
+    (setq mouse-wheel-scroll-amount '(1
+                                      ((shift) . 5)
+                                      ((control))))
+    (dolist (multiple '("" "double-" "triple-"))
+      (dolist (direction '("right" "left"))
+        (global-set-key (read-kbd-macro (concat "<" multiple "wheel-" direction ">")) 'ignore)))
+    (global-set-key (kbd "M-v") 'yank)
+    (global-set-key (kbd "M-c") 'kill-ring-save)
+    (global-set-key (kbd "M-`") 'ns-next-frame)
+    (global-set-key (kbd "M-h") 'ns-do-hide-emacs)
+    (global-set-key (kbd "M-˙") 'ns-do-hide-others)
+    (with-eval-after-load 'nxml-mode
+      (define-key nxml-mode-map (kbd "M-h") nil))
+    (global-set-key (kbd "M-ˍ") 'ns-do-hide-others) ;; what describe-key reports for cmd-option-h
+    )
+
+(setq initial-major-mode 'fundamental-mode)  ; default mode for the *scratch* buffer
+(setq display-time-default-load-average nil) ; this information is useless for most
+
+;; Automatically reread from disk if the underlying file changes
+(setq auto-revert-interval 1)
+(setq auto-revert-check-vc-info t)
+(global-auto-revert-mode)
+
+;; Save history of minibuffer
+(savehist-mode)
+
+;; Fix scroll margin
+(pixel-scroll-precision-mode)
+(setq scroll-margin 6
+      scroll-conservatively 0
+      scroll-up-aggressively 0.01
+      scroll-down-aggressively 0.01)
+(setq-default scroll-up-aggressively 0.01
+              scroll-down-aggressively 0.01)
+
+;; Line numbers
+(setq line-number-mode t)                        ; Show current line in modeline
+(setq column-number-mode t)                      ; Show column as well
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+
+;; Fix show-paren delay
+(setq show-paren-delay 0.0)
+(show-paren-mode 1)
+
+
+;; Straight
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
@@ -12,73 +86,18 @@
   (load bootstrap-file nil 'nomessage))
 
 (straight-use-package 'use-package)
-(setq inhibit-startup-message t
-      visible-bell nil
-      use-package-always-ensure t
-      straight-use-package-by-default t
-      native-comp-async-report-warnings-errors nil
-      compilation-read-command nil ;; Don't ask for compile command
-      eldoc-echo-area-use-multiline-p nil)
 
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(menu-bar-mode -1)
-(blink-cursor-mode 0)
+(use-package straight
+  :custom
+  (straight-use-package-by-default t))
 
-;; Fix scrolling
-(setq mouse-wheel-scroll-amount '(3 ((shift) . 3))) ;; one line at a time
-(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
-(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
-
-(setq scroll-margin 6
-      scroll-conservatively 0
-      scroll-up-aggressively 0.01
-      scroll-down-aggressively 0.01)
-(setq-default scroll-up-aggressively 0.01
-              scroll-down-aggressively 0.01)
-
-;; Font
-(add-to-list 'default-frame-alist '(font . "Menlo-13"))
-
-(setq show-paren-delay 0.0)
-(show-paren-mode 1)
-
-(use-package no-littering)
-
-(use-package string-inflection
-  :after evil
+;; Fix macOS PATH variable
+(use-package exec-path-from-shell
   :config
-  (define-key evil-normal-state-map (kbd "grc") 'string-inflection-lower-camelcase)
-  (define-key evil-normal-state-map (kbd "grs") 'string-inflection-underscore)
-  (define-key evil-normal-state-map (kbd "grp") 'string-inflection-camelcase))
+  (exec-path-from-shell-initialize))
 
-;; Auto-close compilation buffer
-(defun felipe/bury-compile-buffer-if-successful (buffer string)
-  "Bury a compilation buffer if succeeded without warnings "
-  (if (and
-       (string-match "compilation" (buffer-name buffer))
-       (string-match "finished" string)
-       (not
-        (with-current-buffer buffer
-          (search-forward "warning" nil t))))
-      (run-with-timer 1 nil
-                      (lambda (buf)
-                        (bury-buffer buf)
-                        (switch-to-prev-buffer (get-buffer-window buf) 'kill))
-                      buffer)))
-(add-hook 'compilation-finish-functions 'felipe/bury-compile-buffer-if-successful)
-
-;; Set compile command
-(defun felipe/set-compile-command (proj-file command-str)
-  (let ((proj-dir (locate-dominating-file default-directory proj-file)))
-    (when proj-dir
-        (set (make-local-variable 'compile-command) (format command-str proj-dir)))))
-
-;; Remove vc-handled-backends when file is remote
-(defun felipe/vc-off-if-remote ()
-  (if (file-remote-p (buffer-file-name))
-      (setq-local vc-handled-backends nil)))
-(add-hook 'find-file-hook 'felipe/vc-off-if-remote)
+;; No littering
+(use-package no-littering)
 
 ;; Zooming
 (use-package default-text-scale
@@ -87,20 +106,28 @@
          ("C--" . default-text-scale-decrease)
          ("C-0" . default-text-scale-reset)))
 
-(use-package magit
-  :defer t
+;; Evil
+(use-package undo-tree
+  :ensure t
+  :init
+  (setq undo-tree-auto-save-history t
+        undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo")))
   :config
-  (evil-define-key 'normal 'magit-mode-map (kbd "C-j") 'evil-window-next)
-  (evil-define-key 'normal 'magit-mode-map (kbd "C-k") 'evil-window-prev))
+  (global-undo-tree-mode)
+  (with-eval-after-load 'undo-tree (defun undo-tree-overridden-undo-bindings-p () nil)))
 
 (use-package evil
+  :after undo-tree
+  :ensure t
+
   :init
-  (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
-  (setq evil-want-keybinding nil)
+  (setq evil-want-integration t ;; This is optional since it's already set to t by default.
+	evil-want-keybinding nil)
+
   :config
   (evil-mode 1)
+  (evil-set-undo-system 'undo-tree)
 
-  ;; Overload shifts so that they don't lose the selection
   (defun felipe/evil-shift-left-visual ()
     (interactive)
     (evil-shift-left (region-beginning) (region-end))
@@ -118,148 +145,146 @@
 
   (define-key key-translation-map (kbd "ESC") (kbd "C-g"))
 
+  (eval-after-load "dired" '(progn
+    (define-key evil-normal-state-map (kbd "-") 'dired-jump)
+    (define-key dired-mode-map (kbd "-") 'dired-up-directory)))
+
   (define-key evil-normal-state-map (kbd "C-b") 'switch-to-buffer)
-  (define-key evil-normal-state-map (kbd "C-p") 'find-file-in-project)
+  (define-key evil-normal-state-map (kbd "C-p") 'project-find-file)
   (define-key evil-normal-state-map (kbd "C-a") 'ff-find-other-file)
 
   (define-key evil-normal-state-map (kbd "C-j") 'evil-window-next)
   (define-key evil-normal-state-map (kbd "C-k") 'evil-window-prev)
 
-  (define-key evil-normal-state-map (kbd "<f7>") 'compile)
-
   (define-key evil-insert-state-map (kbd "C-n") 'completion-at-point)
   (define-key evil-insert-state-map (kbd "C-p") 'completion-at-point))
 
-(use-package evil-collection
-  :after evil
-  :ensure t
-  :config
-  (evil-collection-init 'magit))
-
-(use-package evil-surround
-  :after evil
-  :config
-  (global-evil-surround-mode 1))
-
 (use-package evil-commentary
+  :ensure t
   :after evil
   :config
   (evil-commentary-mode))
 
-(use-package undo-tree
+(use-package evil-surround
+  :ensure t
+  :after evil
   :config
-  (global-undo-tree-mode)
-  (evil-set-undo-system 'undo-tree))
+  (global-evil-surround-mode 1))
+
+(use-package evil-collection
+  :ensure t
+  :after evil
+  :config
+  (evil-collection-init))
+
+(use-package string-inflection
+  :after evil
+  :config
+  (define-key evil-normal-state-map (kbd "grc") 'string-inflection-lower-camelcase)
+  (define-key evil-normal-state-map (kbd "grs") 'string-inflection-underscore)
+  (define-key evil-normal-state-map (kbd "grp") 'string-inflection-camelcase))
 
 (use-package general
+  :ensure t
   :config
-  (general-create-definer felipe/leader-def :prefix "SPC")
+  ;; allow for shorter bindings -- e.g., just using things like nmap alone without general-* prefix
+  (general-evil-setup t)
+  (general-auto-unbind-keys)
+
+  (general-create-definer felipe/leader-def
+      :states '(normal)
+      :keymaps 'override
+      :prefix "SPC")
+
   (felipe/leader-def
-   :keymaps 'normal
+     "w" '(:ignore t :which-key "window")
+     "w/" '(split-window-right :which-key "split right")
+     "w-" '(split-window-below :which-key "split below")
+     "wd" '(delete-window :which-key "delete window")
+     "wb" '(balance-windows :which-key "balance windows")
+  
+     "p" '(:ignore t :which-key "project")
+     "pp" '(project-switch-project :which-key "switch project")
+  
+     "f" '(:ignore t :which-key "file")
+     "fg" 'counsel-rg
+     "fe" '((lambda () (interactive) (find-file "~/.emacs.d/init.el")) :which-key "emacs config")
+  
+     "b" '(:ignore t :which-key "buffer")
+     "bd" 'kill-this-buffer
+     "bn" 'next-buffer
+     "bp" 'previous-buffer
+  
+     "g" '(:ignore t :which-key "git")
+     "gs" 'magit-status
+  
+     ;; "mf" 'eglot-format-buffer
+     ;; "mr" 'eglot-rename
+     ;; "mi" 'eglot-code-actions
+   )
+)
 
-   "w/" 'split-window-right
-   "w-" 'split-window-below
-   "wd" 'delete-window
-   "wb" 'balance-windows
-
-   "pp" 'project-switch-project
-
-   "fg" 'consult-ripgrep
-   "fed" (lambda () (interactive) (find-file "~/.emacs.d/init.el"))
-
-   "bd" 'kill-this-buffer
-   "bn" 'next-buffer
-   "bp" 'previous-buffer
-
-   "gs" 'magit-status
-
-   "mf" 'eglot-format-buffer
-   "mr" 'eglot-rename
-   "mi" 'eglot-code-actions))
-
-(use-package savehist
-  :init
-  (savehist-mode))
-
-(use-package consult)
-
-(use-package orderless
-  :custom
-  (completion-styles '(orderless basic))
-  (completion-category-overrides '((file (styles basic partial-completion)))))
-
-(use-package corfu
-  :init
-  (global-corfu-mode)
+;; Which-key
+(use-package which-key
+  :ensure t
   :config
-  (general-def
-    :keymaps 'completion-in-region-mode
-    :definer 'minor-mode
-    :states 'insert
-    :predicate 'corfu-mode
-    "C-n" 'corfu-next
-    "C-p" 'corfu-previous
-    "<escape>" 'corfu-quit
-    "<return>" 'corfu-insert))
+  (which-key-mode))
 
-(use-package corfu-terminal
-  :after corfu
-  :init (corfu-terminal-mode))
+;; Ivy/counsel
+(use-package ivy
+  :ensure t
+  :config
+  (ivy-mode))
+(use-package counsel
+  :ensure t)
 
-(use-package marginalia
-  :init (marginalia-mode))
+;; Org-mode
+(use-package org
+  :ensure t)
 
-(use-package vertico
-  :init (vertico-mode))
+;; Magit
+(use-package magit
+  :ensure t)
 
-(use-package find-file-in-project)
-
-;; (use-package modus-themes
-;;   :config
-;;   (load-theme 'modus-vivendi t))
-
+;; Theme
 (use-package doom-themes
+  :ensure t
   :config
-  (load-theme 'doom-opera t))
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-homage-black t)
 
-(use-package doom-modeline
-  :hook (after-init . doom-modeline-mode))
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
 
-(use-package eglot
-  :defer t
-  :hook ((go-mode . eglot-ensure)
-	 (c-mode . eglot-ensure)
-	 (c++-mode . eglot-ensure)
-	 (typescript-mode . eglot-ensure)
-	 (web-mode . eglot-ensure)))
+;; Auto-close compilation buffer
+(defun felipe/bury-compile-buffer-if-successful (buffer string)
+  "Bury a compilation buffer if succeeded without warnings "
+  (if (and
+       (string-match "compilation" (buffer-name buffer))
+       (string-match "finished" string)
+       (not
+        (with-current-buffer buffer
+          (search-forward "warning" nil t))))
+      (run-with-timer 1 nil
+                      (lambda (buf)
+                        (bury-buffer buf)
+                        (switch-to-prev-buffer (get-buffer-window buf) 'kill))
+                      buffer)))
+(add-hook 'compilation-finish-functions 'felipe/bury-compile-buffer-if-successful)
 
-;; C/C++ config
-(defun felipe/c-c++-hook ()
-  (felipe/set-compile-command "CMakeLists.txt" "cd %1$s/build && cmake --build %1$s/build")
-  (felipe/set-compile-command "meson.build" "ninja -C %s/build")
-  (felipe/set-compile-command "Makefile" "make -C %s")
-  (felipe/set-compile-command "makefile" "make -C %s"))
-
-(add-hook 'c++-mode-hook 'felipe/c-c++-hook)
-(add-hook 'c-mode-hook 'felipe/c-c++-hook)
-
-;; Go config
-(use-package go-mode
-  :defer t
+;; Coq
+(use-package proof-general
+  :ensure t
+  :init
+  (setq proof-splash-enable nil)
   :config
-  (add-hook 'go-mode-hook (lambda ()
-			    (setq tab-width 4)
-			    (felipe/set-compile-command "makefile" "make -C %s")
-			    (felipe/set-compile-command "Makefile" "make -C %s"))))
+  (add-hook 'coq-mode-hook (lambda () (undo-tree-mode 1))))
 
-(use-package graphql-mode
-  :defer t)
 
-(use-package web-mode
-  :defer t
-  :mode (("\\.js\\'" . web-mode)
-         ("\\.jsx\\'" .  web-mode)
-         ("\\.ts\\'" . web-mode)
-         ("\\.tsx\\'" . web-mode)
-         ("\\.html\\'" . web-mode)
-         ("\\.svelte\\'" . web-mode)))
+;; Ocaml
+(use-package tuareg
+  :ensure t)
