39a103c010eead5a1153e8897f7a38e0f1fa4f0b
Felipe A. Costa <felipeagc2@gmail.com>
2025-08-10 18:45:06 -0300

    Handle treesitter updates

--- nvim/.config/nvim/lua/felipeagc/treesitter.lua
+++ nvim/.config/nvim/lua/felipeagc/treesitter.lua
@@ -41,3 +41,20 @@ require("nvim-treesitter.configs").setup({
         },
     },
 })
+
+vim.api.nvim_create_autocmd('PackChanged', {
+  desc = 'Handle nvim-treesitter updates',
+  group = vim.api.nvim_create_augroup('nvim-treesitter-pack-changed-update-handler', { clear = true }),
+  callback = function(event)
+    if event.data.kind == 'update' then
+      vim.notify('nvim-treesitter updated, running TSUpdate...', vim.log.levels.INFO)
+      ---@diagnostic disable-next-line: param-type-mismatch
+      local ok = pcall(vim.cmd, 'TSUpdate')
+      if ok then
+        vim.notify('TSUpdate completed successfully!', vim.log.levels.INFO)
+      else
+        vim.notify('TSUpdate command not available yet, skipping', vim.log.levels.WARN)
+      end
+    end
+  end,
+})
