c7ad2dd94d151ebeb110eacd92b2b9b218b9ba79
Felipe A. Costa <felipeagc2@gmail.com>
2025-11-10 19:44:02 -0300

    Add hidden desktop entries

--- setup_arch.sh
+++ setup_arch.sh
@@ -7,6 +7,7 @@ pikaur -S --needed - < aur_packages.txt
 
 mkdir -p ~/.config
 mkdir -p ~/.local/bin
+mkdir -p ~/.local/share/applications
 
 stow nvim
 stow ghostty-linux
@@ -18,6 +19,7 @@ stow jj
 stow tmux
 stow linux-bin
 stow uwsm
+stow xdg
 
 elephant service enable
 systemctl enable --user elephant
@@ -30,3 +32,10 @@ sudo tee /etc/sddm.conf.d/autologin.conf > /dev/null <<EOF
 User=$USER
 Session=hyprland-uwsm
 EOF
+
+if [ ! -f $HOME/.local/share/applications/ChatGPT.desktop ]; then
+    omarchy-webapp-install ChatGPT "https://chatgpt.com" "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/chatgpt.png"
+fi
+if [ ! -f $HOME/.local/share/applications/WhatsApp.desktop ]; then
+    omarchy-webapp-install WhatsApp "https://web.whatsapp.com" "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/whatsapp.png"
+fi
