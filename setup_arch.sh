44a27d9d282bd600a0c33371e997ed499644b66e
Felipe A. Costa <felipeagc2@gmail.com>
2025-11-10 00:18:32 -0300

    More linux updates

--- setup_arch.sh
+++ setup_arch.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-sudo pacman -S - < arch_packages.txt
+sudo pacman -S --needed - < arch_packages.txt
 
-gpg --recv-keys 58117AFA1F85B3EEC154677D615D449FE6E6A235
-gpg --recv-keys 63CBEEC9006602088F9B19326224F9941A8AA6D1
-gpg --recv-keys E46E6F8FF02E4C83569084589239277F560C95AC
+pikaur -S --needed - < aur_packages.txt
 
-pikaur -S - < aur_packages.txt
-
-
+elephant service enable
+systemctl enable --user elephant
+systemctl start --user elephant
