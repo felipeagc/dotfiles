5676a5d2bd49b8e84fd5076b0d28a3ccc045b6b8
Felipe A. Costa <felipeagc2@gmail.com>
2026-04-05 22:37:10 -0300

    Add home-manager

---
+++ home-manager/.config/home-manager/flake.nix
@@ -0,0 +1,58 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    # neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+      nixpkgs,
+      # neovim-nightly-overlay,
+      home-manager,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ 
+          # neovim-nightly-overlay.overlays.default
+        ];
+      };
+    in {
+      homeConfigurations.felipe = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [{
+          home.username = "felipe";
+          home.homeDirectory = "/home/felipe";
+          home.stateVersion = "24.11";
+
+          home.packages = with pkgs; [
+            neovim
+            jujutsu
+            jjui
+            lazygit
+            fzf
+            ripgrep
+            eternal-terminal
+            tmux
+            nodejs_24
+          ];
+
+          systemd.user.services.etserver = {
+            Unit.Description = "Eternal Terminal Server";
+            Service = {
+              ExecStart = "${pkgs.eternal-terminal}/bin/etserver";
+              Restart = "on-failure";
+            };
+            Install.WantedBy = [ "default.target" ];
+          };
+
+          programs.home-manager.enable = true;
+        }];
+      };
+    };
+}
